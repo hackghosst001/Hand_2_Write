374996b267373d2d6dcc31c486428166cf17765e
Ansu_Raj <ansuraj1998rks@gmail.com>
2025-04-30 01:42:32 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/s')
+def name():
+    return "i am able to run python"
+
+
+if __name__ == '__main__':
+    app.run(debug=True,)
+
+
+
+
+
